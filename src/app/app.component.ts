b4245be104e2d47e195c89ae711fe82f1661223b
angelSantori <antonyo.da.mm@gmail.com>
2023-11-01 09:36:58 -0700

    App1 Completa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //Título
   title = 'app1Angular';
+
+  //Directivas nglf & ngFor
+  nombre = 'Rodriguez Pablo';
+  edad = 40;
+  sueldos = [1700, 1600, 1900];
+
+  //Captura de eventos
+  contador = 1;
+
+  incrementar() {
+    this.contador++;
+  }
+
+  decrementar() {
+    this.contador--;
+  }
+
+  //Directiva ngModel
+  nombre2='';
+  apellido='';
 }
